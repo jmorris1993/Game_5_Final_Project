982654cf8b4b233ed3fde4159ef2fd8e4920e807
jmorris1993 <julian.morris@students.olin.edu>
2014-12-05 14:29:21 -0500

    added root/thing

---
+++ Thing.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 05 14:26:18 2014
+
+@author: jmorris
+"""
+
+#
+# The root object
+#
+class Root (object):
+    # default predicates
+
+    # is this object a Thing?
+    def is_thing (self):
+        return False
+
+    # is this object a Character?
+    def is_character (self):
+        return False
+
+    # is this object the Player?
+    def is_player (self):
+        return False
+
+    # can this object be walked over during movement?
+    def is_walkable (self):
+        return False
+
+
+# A thing is something that can be interacted with and by default
+# is not moveable or walkable over
+#
+#   Thing(name,description)
+#
+# A thing defines a default sprite in field _sprite
+# To create a new kind of thing, subclass Thing and 
+# assign it a specific sprite (see the OlinStatue below).
+# 
+class Thing (Root):
+    def __init__ (self,name,desc):
+        self._name = name
+        self._description = desc
+        self._sprite = Text(Point(TILE_SIZE/2,TILE_SIZE/2),"?")
+        log("Thing.__init__ for "+str(self))
+
+    def __str__ (self):
+        return "<"+self.name()+">"
+
+    # return the sprite for display purposes
+    def sprite (self):
+        return self._sprite
+
+    # return the name
+    def name (self):
+        return self._name
+
+    # return the position of the thing in the level array
+    def position (self):
+        return (self._x,self._y)
+        
+    # return the description
+    def description (self):
+        return self._description
